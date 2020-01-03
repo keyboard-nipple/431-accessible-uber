12b83905110fb0003d8ae56e0d3aeaef25ca747e
Kevin Jiang <kjiang1998@gmail.com>
2020-01-03 03:52:05 +0000

    Got development environment working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # Disables blocked host feature in development environment
+  config.hosts = nil
 end
