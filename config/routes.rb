d5cae03c8d79c0e844365434efd3cc177e871b71
Kevin Jiang <kjiang1998@gmail.com>
2020-01-07 11:16:52 +0000

    Linked create rider account button to new rider page

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  # root 'welcome#index'
-  root 'riders#index'
+  root 'welcome#index'
+  # root 'riders#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
