52576fa3fc5302f37fecb96f67ba3498af9e4ee2
Ubuntu <ubuntu@ip-172-31-42-89.us-east-2.compute.internal>
2020-01-08 01:26:15 +0000

    Driver controller and model

---
+++ db/migrate/20200108004816_create_drivers.rb
@@ -0,0 +1,13 @@
+class CreateDrivers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :drivers do |t|
+      t.string :name
+      t.string :email
+      t.string :dob
+      t.string :license#
+      t.string :uid
+
+      t.timestamps
+    end
+  end
+end
